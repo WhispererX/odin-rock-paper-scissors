1ea6873306586e432d51fc8de735f58d84657276
Whisperer <whisperer.meta@gmail.com>
2025-08-07 19:32:28 +0300

    Add playGame function
    Implements the entire game logic

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // If the number is below 0.3, set cpuChoice to "rock"
 // Else if it's below 0.6, set cpuChoice to "paper"
 // Else set cpuChoice to "scissors"
+// print outcome
 // return cpuChoice
 
 function getComputerChoice() {
@@ -21,6 +22,7 @@ function getComputerChoice() {
         cpuChoice = 'scissors';
     }
 
+    console.log(`Computer chose ${cpuChoice}!`);
     return cpuChoice;
 }
 
@@ -31,5 +33,98 @@ function getComputerChoice() {
 // return their choice
 
 function getHumanChoice() {
-    return prompt('Choose rock, paper, or scissors');
+    const choice = prompt('Choose rock, paper, or scissors');
+    console.log(`You chose ${choice}!`);
+    return choice;
 }
+
+// create a new function named playRound
+// Define two parameters for playRound: humanChoice and cpuChoice.
+// Make function’s humanChoice parameter case-insensitive
+
+// playRound
+// humanChoice.toLowerCase();
+// cpuChoice.toLowerCase();
+// switch humanChoice + cpuChoice
+// case rockpaper: cpu wins, increment cpu score, log outcome
+// case rockscissors: user wins, increment user score, log outcome
+// case paperrock: user wins, increment user score, log outcome
+// case paperscissors: cpu wins, increment cpu score, log outcome
+// case scissorspaper: user wins, increment user score, log outcome
+// case scissorsrock: cpu wins, increment cpu score, log outcome
+// default: tie, log outcome
+
+function playGame() {
+    const rounds = 5;
+
+    // keep track of user score
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function humanWins(humanChoice, cpuChoice) {
+        console.log(`You win! ${humanChoice} beats ${cpuChoice}.`);
+        humanScore++;
+    }
+    function cpuWins(humanChoice, cpuChoice) {
+        console.log(`You lose! ${cpuChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+
+    function tie() {
+        console.log('It was a tie!');
+    }
+
+    function playRound(humanChoice, cpuChoice) {
+        let humanResult = humanChoice.toLowerCase();
+        let cpuResult = cpuChoice.toLowerCase();
+
+        switch (humanResult + cpuResult) {
+            case 'rockpaper':
+                cpuWins(humanChoice, cpuChoice);
+                break;
+
+            case 'rockscissors':
+                humanWins(humanChoice, cpuChoice);
+                break;
+
+            case 'paperrock':
+                humanWins(humanChoice, cpuChoice);
+                break;
+
+            case 'paperscissors':
+                cpuWins(humanChoice, cpuChoice);
+                break;
+
+            case 'scissorspaper':
+                humanWins(humanChoice, cpuChoice);
+                break;
+
+            case 'scissorsrock':
+                cpuWins(humanChoice, cpuChoice);
+                break;
+
+            default:
+                tie();
+                break;
+        }
+    }
+
+    for (let round = 1; round < rounds; round++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log(`You won ${humanScore} rounds out of 5`);
+    if (humanScore > computerScore) {
+        console.log('You won the game!');
+    } else {
+        console.log('You lost the game!');
+    }
+
+    if (confirm('Play again?')) {
+        playGame();
+    }
+}
+
+playGame();
